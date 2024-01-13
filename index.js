56e2e2bf77e2381d9158ad06af1ea673dcf20c7d
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-13 10:56:13 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(spy) {
+  const receivesAFunction = spy; 
+  return spy();
+}
+
+function returnsANamedFunction() {
+  return function fn() {
+    var fn !== returnsANamedFunction()
+  };
+}
+ 
+
+function returnsAnAnonymousFunction() {
+  return () => {
+    var fn === returnsANamedFunction()
+  };
+ };
